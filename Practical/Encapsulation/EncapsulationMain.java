1f65accff0133945ce405dfc1d0e7debb4e608c5
SHIVAMRAJESHGUPTA <112972657+SHIVAMRAJESHGUPTA@users.noreply.github.com>
2022-10-08 08:57:08 +0530

    Add files via upload

---
+++ Practical/Encapsulation/EncapsulationMain.java
@@ -0,0 +1,19 @@
+package Shivam;
+
+public class EncapsulationMain {
+	
+	public static void main(String[] args)
+	{
+		Encapsulation p = new Encapsulation();
+		 p.setName("Shivam");
+		 p.setAge(21);
+		 p.setGender("Male");
+		 
+		 System.out.println("Name is : "+p.getName());
+		 System.out.println("Age is : "+p.getAge());
+		 System.out.println("Gender is : "+p.getGender());
+		 
+		 
+	}
+
+}
