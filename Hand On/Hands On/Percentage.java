5a85f8aab336c8b99bc475c8158aa1cb9688763f
SHIVAMRAJESHGUPTA <112972657+SHIVAMRAJESHGUPTA@users.noreply.github.com>
2022-10-11 15:20:45 +0530

    Update Percentage.java

--- Hand On/Hands On/Percentage.java
+++ Hand On/Hands On/Percentage.java
@@ -1,9 +1,12 @@
+//Demonstate if-else statement of 5 Subject The Person who gets above 60% will be pass other wise fail
+//Name :- Shivam_Gupta
+//Date :- 11-10-22
 package Shivam;
 import java.util.Scanner;
 public class Percentage {
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);	// Taken input from User Taking 5 Subject Marks from user   
 		System.out.println("Enter your Name");
 		String Name = sc.nextLine();
 		System.out.println("Enter English Mark");
@@ -17,7 +20,7 @@ public class Percentage {
 		System.out.println("Enter History Mark");
 		int History = sc.nextInt();
 		
-		int Percentage = (English+Maths+Science+Geog+History)/5;
+		int Percentage = (English+Maths+Science+Geog+History)/5;// Taking percent  
 		System.out.println("percentage : "+Percentage);
 		if( Percentage>=60)
 		{
