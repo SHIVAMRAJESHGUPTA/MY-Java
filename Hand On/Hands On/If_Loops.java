79640d8a28437756af444aa993141ed7eb98689f
SHIVAMRAJESHGUPTA <112972657+SHIVAMRAJESHGUPTA@users.noreply.github.com>
2022-10-11 15:15:53 +0530

    Update If_Loops.java

--- Hand On/Hands On/If_Loops.java
+++ Hand On/Hands On/If_Loops.java
@@ -1,18 +1,21 @@
+//Test Whether the person is eligible or not for vote
+//Name :- Shivam_Gupta
+//Date :- 11-10-22
 package Shivam;
 import java.util.Scanner;
 public class If_Loops {
 
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //taking input from user 
 		System.out.println("Enter the age : ");
 		int person= sc.nextInt();
 		
-		if(person >=18)
+		if(person >=18)		//if it is eligible //if else loop 
 		{
 			System.out.println("THIS PERSON CAN VOTE ");
 		}
-		else
+		else		// if it is not eligible 
 		{
 			System.out.println("Not eligible ");
 		}
