6ece1c5a7df4e41a304eb507d6bcbfc45884b53d
SHIVAMRAJESHGUPTA <112972657+SHIVAMRAJESHGUPTA@users.noreply.github.com>
2022-10-14 13:44:20 +0530

    Add files via upload

---
+++ Hand On/Typecasting.java
@@ -0,0 +1,23 @@
+/* wap on Downcasting
+ * Name : Shivam_Gupta
+ * Date : 14-10-22
+ */
+
+package Shivam;
+import java.util.*;
+public class Typecasting {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int i = sc.nextInt();
+		long l = i;
+		float f = l;			
+		System.out.println("int value is "+i);
+		System.out.println("long value is "+l);
+		System.out.println("float value is "+f);
+
+
+	}
+}
